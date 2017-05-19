7a7e3a519292c44636317cb61c0746db23e9bbbf
Srikar Ananthula <ananthulasrikar@gmail.com>
2017-05-19 13:10:25 +0530

    updated

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	System.out.println($ENV);
+    	System.out.println("$ENV");
         System.out.println( "Hello World!" );
     }
 }
